76f0dae8fee52b544a037da690d53a2810abc2d9
Andreas Kaufhold <andreaskaufhold@mytum.de>
2017-11-06 10:02:53 +0100

    Hello User is added

--- src/HelloUser.java
+++ src/HelloUser.java
@@ -1,6 +1,6 @@
 
 public class HelloUser {
-	
+	//?
 	private String userName;
 	
 	public HelloUser (String userName) {
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub??
+		// TODO Auto-generated method stub???
 		System.out.println("Hello ASE2017 - How are you? :)");
 		
 		System.out.println("Please enter your name: ");
